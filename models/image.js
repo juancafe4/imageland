34bce7bfe541fe038d84305cfc6f2a697e2b4f33
Juan Carlos <juancafe2@gmail.com>
2016-08-06 13:01:08 -0700

    Table created in node

--- models/image.js
+++ models/image.js
@@ -3,3 +3,16 @@ const squel = require('squel');
 const connection = require('../config/db');
 
 
+//Initialize the table 
+
+connection.query(`create table if not exists images (
+    id varchar(50),
+    url varchar(100),
+    title varchar(100),
+    desc varchar(500),
+    createdAt timestamp
+  )`, err => {
+    if (err) throw err;
+  });
+
+
