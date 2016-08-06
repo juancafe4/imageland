16849d28967ad431804be1d0de8bfee2bdf88ead
Juan Carlos <juancafe2@gmail.com>
2016-08-06 13:37:45 -0700

    Added a create fuction

--- models/image.js
+++ models/image.js
@@ -2,7 +2,8 @@
 const squel = require('squel').useFlavour('mysql');
 const connection = require('../config/db');
 
-
+const uuid = require('uuid')
+const momeent = require('moment')
 //Initialize the table 
 
 connection.query(`create table if not exists images (
@@ -19,12 +20,26 @@ exports.getAll = function() {
   return new Promise((resolve, reject) => {
     let sql = squel.select().from('images').toString();
     connection.query(sql, (err, images) => {
-      if (err) throw err
+      if (err) resolve(err)
       else resolve(images)
     });
   });
 }
 
+exports.create =  function(newImage) {
+  return new Promise((resolve, reject) => {
+    let sql = squel.insert()
+                  .into('images')
+                  .setFields(newImage)
+                  .set('id', uuid.v4())
+                  .set('createdAt', moment().format('YYYY/MM/DD HH:mm:ss'))
+                  .toString();
+    connection.query(sql, err => {
+      if(err) reject(err)
+      else resolve()
+    });
+  });
+}
 
 //// LATER
 // Image.getAll.then()....
