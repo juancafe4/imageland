d16662e557a56e1214b202ddaf74a5eeff043dc4
Juan Carlos <juancafe2@gmail.com>
2016-08-06 13:16:25 -0700

    We can get all the images

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require ('express');
 const morgan  = require('morgan');
 const bodyParser = require('body-parser')
-
+const Image = require('./models/image')
 const app = express();
 //Port
 const port = process.env.PORT | 8000
@@ -17,6 +17,15 @@ app.use(express.static('public'));
 
 //ROUTES
 
+app.get('/images', (req, res) => {
+  Image.getAll()
+    .then(images => {
+      res.send(images)
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    });
+});
 //Server listen
 app.listen(port), err => {
   console.log(err || `Listenning at ${port}`);
--- models/image.js
+++ models/image.js
@@ -1,5 +1,5 @@
 //Image model
-const squel = require('squel');
+const squel = require('squel').useFlavour('mysql');
 const connection = require('../config/db');
 
 
@@ -9,10 +9,22 @@ connection.query(`create table if not exists images (
     id varchar(50),
     url varchar(100),
     title varchar(100),
-    desc varchar(500),
+    description varchar(500),
     createdAt timestamp
   )`, err => {
     if (err) throw err;
   });
 
+exports.getAll = function() {
+  return new Promise((resolve, reject) => {
+    let sql = squel.select().from('images').toString();
+    connection.query(sql, (err, images) => {
+      if (err) throw err
+      else resolve(images)
+    });
+  });
+}
 
+
+//// LATER
+// Image.getAll.then()....
