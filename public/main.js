2c2a448df3adbc6cb44a2abd09cdcd5eb93f513a
Juan Carlos <juancafe2@gmail.com>
2016-08-06 16:52:05 -0700

    fix bug related to modal

--- public/main.js
+++ public/main.js
@@ -1,9 +1,36 @@
 $(() => {
   console.log('Ready!')
   renderAllImages()
-
+  $('.images').on('click', '.edit', openEditModal)
+  $('#myEditModal').find('form').on('submit', ".edit", saveEdit)
 })
 
+fucntion saveEdit(e) {
+  event.preventDefault()
+  let $editModal = $('#myEditModal')
+
+  let id = $editModal.data('editingId')
+    //Getting info for the modal
+  let title =  $editModal.find('.title').text()
+  let description = $editModal.find('.description').text()
+  let imageURL = $editModal.find('img').attr('src') 
+
+  $.ajax( {
+    url: `/image/${id}`,
+    method: 'PUT',
+    data: {url, title, description}
+  })
+  .done(newImage => {
+    console.log('newImage: ', newImage)
+    renderAllImages()
+  })
+  .fail(err => {
+    console.log('err: ', err)
+  })
+  .always(() => {
+    $editModal.modal('hide')
+  })
+}
 //Request all images and put it into the DOM
 function renderAllImages() {
   $.get('/images')
@@ -21,9 +48,28 @@ function renderAllImages() {
         return $image;
       });
 
-      $('.images').append($medias)
+      $('.images').empty().append($medias)
     })
     .fail(err => {
       console.log('err: ', err);
+
     });
+}
+
+function openEditModal(event) {
+  let $editButton = $(event.target)
+  let $myEditModal = $editButton.closest('.media')
+  let $media = $myEditModal.closest()
+  //Getting info for the modal
+  let title =  $media.find('.title').text()
+  let description = $media.find('.description').text()
+  let imageURL = $media.find('img').attr('src')
+  let id = $('.media').data('id')
+  console.log('event.target', event.target)
+  
+  $myEditModal.data("editingId", id)
+  $('#myEditModal').modal( {
+    keyboard: false,
+    backdrop: 'static'
+  });
 }
